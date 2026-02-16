17213029bcfcb373127bab126d1cc4d66cc87b08
Phanikarthik919 <phanikarthikkandukoori@gmail.com>
2026-02-16 10:54:35 +0530

    Delete Suntek/week-4/Day-1/info.js

--- Suntek/week-4/Day-1/info.js
+++
@@ -1,9 +0,0 @@
-//why are we using schemas?
-//schemas are used for validation
-//validation rules
-//required,maxlength,pattern matching
-//user can save data into database by calling save / insert /insertone methods
-//at the time of insertion and modification the validation is done
-//to handle errors  we need to use default error handling middleware
-
-//projectionssx
